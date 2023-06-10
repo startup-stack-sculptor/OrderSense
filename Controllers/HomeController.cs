99b31a69331c2fec45a3c355e21fc9a8db12ec5a
4mishra <jagada.mishradev@gmail.com>
2023-06-11 00:29:11 +0530

    HomeController

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,31 +1,37 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using OrderSense.Models;
+using coderush.Models;
 
-namespace OrderSense.Controllers;
-
-public class HomeController : Controller
+namespace OrderSense.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public class HomeController : Controller
     {
-        _logger = logger;
-    }
+        public IActionResult Index()
+        {
+            return RedirectToAction("UserProfile", "UserRole");
+        }
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+            return View();
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
