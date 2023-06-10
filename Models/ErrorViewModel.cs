2ca1c722734b90840a76f89f46f5b833c7104a21
4mishra <jagada.mishradev@gmail.com>
2023-06-11 00:24:08 +0530

    ErrorViewModel model

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -1,8 +1,11 @@
-namespace OrderSense.Models;
+using System;
 
-public class ErrorViewModel
+namespace OrderSense.Models
 {
-    public string? RequestId { get; set; }
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
 
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
 }
